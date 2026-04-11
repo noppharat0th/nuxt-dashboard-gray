03db3b8fe4bfc90956e12857138621b04c2df4ea
Nopparatz1238 <uiavgcgccggv@gmail.com>
2026-04-12 01:29:34 +0700

    update component tab-menu

---
+++ app/components/layouts/tab-menu.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items = ref<NavigationMenuItem[][]>([
+  [
+    {
+      label: 'Guide',
+      icon: 'i-lucide-book-open',
+      active: true,
+    },
+    {
+      label: 'Composables',
+      icon: 'i-lucide-database',
+    },
+    {
+      label: 'Components',
+      icon: 'i-lucide-box',
+      to: '/docs/components',
+    }
+  ],
+])
+</script>
+
+<template>
+  <UNavigationMenu
+    highlight
+    highlight-color="primary"
+    orientation="horizontal"
+    :items="items"
+    class="data-[orientation=horizontal]:border-b border-default data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-48"
+  />
+</template>
